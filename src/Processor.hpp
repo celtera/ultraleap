0ba51b30827cf0ba379abd8de8c0eb35380db051
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-04-02 10:57:58 +0200

    Update with lib

--- src/Processor.hpp
+++ src/Processor.hpp
@@ -1,25 +1,25 @@
 #pragma once
 
-#include <avnd/helpers/controls.hpp>
-#include <avnd/helpers/meta.hpp>
+#include <halp/controls.hpp>
+#include <halp/meta.hpp>
 
 #include <vector>
 
 class MyProcessor
 {
 public:
-  $(name, "My Processor")
-  $(c_name, "my_processor")
+  halp_meta(name, "My Processor")
+  halp_meta(c_name, "my_processor")
 
   // CHANGE THIS !!
   // - On linux: uuidgen | xargs printf | xclip -selection clipboard
   //   will copy one on the clipboard
   // - uuidgen exists on Mac and Windows too
-  $(uuid, "00000000-0000-0000-0000-000000000000")
-  $(channels, 2)
+  halp_meta(uuid, "00000000-0000-0000-0000-000000000000")
+  halp_meta(channels, 2)
 
   struct
-  { avnd::hslider_f32<"Gain", avnd::range{.min = 0., .max = 10., .init = 0.5}> gain;
+  { halp::hslider_f32<"Gain", halp::range{.min = 0., .max = 10., .init = 0.5}> gain;
   } inputs;
 
   struct
