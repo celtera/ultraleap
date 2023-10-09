d9aea4b99e91fb82293dde59a70693b123bc973f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-10-09 10:16:02 -0400

    Fix paths

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,19 +1,18 @@
 include(FetchContent)
 
+# Avendish
 FetchContent_Declare(
   avendish
-  #GIT_REPOSITORY "https://github.com/celtera/avendish"
-  #GIT_TAG  6d67263
-  #GIT_PROGRESS true
-  DOWNLOAD_COMMAND ""
-  SOURCE_DIR /Users/jcelerier/projets/avendish
+  GIT_REPOSITORY "https://github.com/celtera/avendish"
+  GIT_TAG main
+  GIT_PROGRESS true
 )
 FetchContent_Populate(avendish)
 
 set(CMAKE_PREFIX_PATH "${avendish_SOURCE_DIR};${CMAKE_PREFIX_PATH}")
 find_package(Avendish REQUIRED)
 
-
+# ConcurrentQueue
 FetchContent_Declare(
   concurrentqueue
   GIT_REPOSITORY https://github.com/cameron314/concurrentqueue.git
@@ -21,8 +20,7 @@ FetchContent_Declare(
 )
 FetchContent_MakeAvailable(concurrentqueue)
 
-
-set(LEAPSDK_PATH /Users/jcelerier/projets/LeapSDK CACHE "" INTERNAL)
+# LeapSDK
 if(NOT LEAPSDK_PATH)
     message(FATAL_ERROR "LEAPSDK_PATH must point to the LeapC SDK folder:
     cmake . -DLEAPSDK_PATH=c:/dev/LeapSDK")
